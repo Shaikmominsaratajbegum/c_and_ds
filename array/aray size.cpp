34755d4e61a5a5135e6b60c3fa02615adc6dafbb
shaikmominsartajbegum <sartajbegam4@gmail.com>
2023-05-14 09:28:45 +0530

    array_in_c

---
+++ array/aray size.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[10];
+    char c[10];
+    float f[10];
+    double d[10];
+    cout<<"size of int"<<sizeof(a);
+    cout<<"size of char"<<sizeof(c);
+    cout<<"size of float"<<sizeof(f);
+    cout<<"size of double"<<sizeof(d);
+}
